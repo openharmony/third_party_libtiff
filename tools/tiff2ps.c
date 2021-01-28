GerHobbelt/libtiff

tiff2ps.c: string literals must be const char *, not char *

tools/tiff2ps.c
@@ -632,7 +632,7 @@ static const char RGBcolorimage[] = "\
  * It is claimed to be part of some future revision of the EPS spec.
  */
 static void
-PhotoshopBanner(FILE* fd, uint32 w, uint32 h, int bs, int nc, char* startline)
+PhotoshopBanner(FILE* fd, uint32 w, uint32 h, int bs, int nc, const char* startline)
 {
 	fprintf(fd, "%%ImageData: %ld %ld %d %d 0 %d 2 \"",
 	    (long) w, (long) h, bitspersample, nc, bs);
@@ -1827,7 +1827,7 @@ PS_Lvl2ImageDict(FILE* fd, TIFF* tif, uint32 w, uint32 h)
 	uint16 predictor, minsamplevalue, maxsamplevalue;
 	uint32 repeat_count;
 	char im_h[64], im_x[64], im_y[64];
-	char * imageOp = "image";
+	const char * imageOp = "image";
 
 	if ( useImagemask && (bitspersample == 1) )
 		imageOp = "imagemask";

openharmony/third_party_libtiff

tools/tiff2ps.c
@@ -632,7 +632,7 @@ static const char RGBcolorimage[] = "\
  * It is claimed to be part of some future revision of the EPS spec.
  */
 static void
-PhotoshopBanner(FILE* fd, uint32_t w, uint32_t h, int bs, int nc, char* startline)
+PhotoshopBanner(FILE* fd, uint32_t w, uint32_t h, int bs, int nc, const char* startline)
 {
 	fprintf(fd, "%%ImageData: %ld %ld %d %d 0 %d 2 \"",
 	    (long) w, (long) h, bitspersample, nc, bs);
@@ -1827,7 +1827,7 @@ PS_Lvl2ImageDict(FILE* fd, TIFF* tif, uint32_t w, uint32_t h)
 	uint16_t predictor, minsamplevalue, maxsamplevalue;
 	uint32_t repeat_count;
 	char im_h[64], im_x[64], im_y[64];
-	char * imageOp = "image";
+	const char * imageOp = "image";
 
 	if ( useImagemask && (bitspersample == 1) )
 		imageOp = "imagemask";